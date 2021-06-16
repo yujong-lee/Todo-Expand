fe045893f8ab27c5797ac003d23873b7da8cd337
yujonglee <yujonglee@g.skku.edu>
2021-06-16 10:42:02 +0900

    새로운 상태구조로 todoSlice와 그 테스트를 다시 작성하라

--- src/redux_module/todoSlice.js
+++ src/redux_module/todoSlice.js
@@ -2,36 +2,6 @@
 
 import { createSlice } from '@reduxjs/toolkit';
 
-function removeTargetFromTask(tasks, id) {
-  const { [id]: deleted, ...rest } = { ...tasks };
-  return rest;
-}
-
-function deleteTaskRecursive(tasks, idToDelete) {
-  if (!tasks) {
-    return {};
-  }
-
-  if (Object.keys(tasks).includes(idToDelete)) {
-    return removeTargetFromTask(tasks, idToDelete);
-  }
-
-  const result = Object.entries(tasks).reduce((acc, [id, task]) => {
-    if (Object.keys(task.children).includes(idToDelete)) {
-      return {
-        ...acc,
-        [id]: {
-          title: task.title,
-          children: removeTargetFromTask(task.children, idToDelete),
-        },
-      };
-    }
-    return { ...acc, [id]: task };
-  }, {});
-
-  return result;
-}
-
 const { actions, reducer } = createSlice({
   name: 'todo',
 
@@ -43,19 +13,29 @@ const { actions, reducer } = createSlice({
 
   reducers: {
     addTask: (state, action) => {
-      if (state.currentTaskId === '0') {
-        state.tasks[state.nextTaskId] = action.payload;
-      } else {
-        state.tasks[state.currentTaskId].children[state.nextTaskId] = action.payload;
-      }
+      const { currentTaskId, nextTaskId } = state;
 
-      state.nextTaskId = (Number.parseInt(state.nextTaskId, 10) + 1).toString(10);
+      state.tasks[nextTaskId] = action.payload;
+
+      state.tasks[currentTaskId].subTasks.push(nextTaskId);
+
+      state.nextTaskId = (Number.parseInt(nextTaskId, 10) + 1).toString(10);
     },
 
-    deleteTask: (state, action) => ({
-      ...state,
-      tasks: deleteTaskRecursive(state.tasks, action.payload),
-    }),
+    deleteTask: (state, action) => {
+      const { payload: idToDelete } = action;
+
+      delete state.tasks[idToDelete];
+
+      Object.keys(state.tasks).forEach(
+        (id) => {
+          const arr = state.tasks[id].subTasks;
+          state.tasks[id].subTasks = arr.filter(
+            (subTaskId) => subTaskId !== idToDelete,
+          );
+        },
+      );
+    },
 
     updateCurrentTaskId: (state, action) => {
       state.currentTaskId = action.payload;
--- src/redux_module/todoSlice.test.js
+++ src/redux_module/todoSlice.test.js
@@ -1,136 +1,44 @@
 import reducer,
-{
-  addTask,
-  deleteTask,
-  updateCurrentTaskId,
-} from './todoSlice';
+{ addTask, deleteTask } from './todoSlice';
 
 describe('todoSlice', () => {
-  context('when currentTaskId is 0', () => {
-    it('adds new task to todoList and updates nextTaskId', () => {
-      const oldState = {
-        currentTaskId: '0',
-        nextTaskId: '3',
-        tasks: { },
-      };
-      const newState = {
-        currentTaskId: '0',
-        nextTaskId: '4',
-        tasks: { 3: { title: '책 읽기', children: {} } },
-      };
+  it('adds new task to todoList and updates nextTaskId', () => {
+    const oldState = {
+      currentTaskId: '0',
+      nextTaskId: '1',
+      tasks: {
+        0: { title: 'root', subTasks: [] },
+      },
+    };
+    const newState = {
+      currentTaskId: '0',
+      nextTaskId: '2',
+      tasks: {
+        0: { title: 'root', subTasks: ['1'] },
+        1: { title: '첫번째 할일', subTasks: [] },
+      },
+    };
 
-      expect(reducer(
-        oldState,
-        addTask({ title: '책 읽기', children: {} }),
-      )).toEqual(newState);
-    });
+    expect(reducer(
+      oldState,
+      addTask({ title: '첫번째 할일', subTasks: [] }),
+    )).toEqual(newState);
   });
 
-  context('when currentTaskId is not 0', () => {
-    it('adds new task to todoList, updates nextTaskId and appends taskId to children array', () => {
-      const oldState = {
-        currentTaskId: '1',
-        nextTaskId: '2',
-        tasks: { 1: { title: '책 읽기', children: {} } },
-      };
+  it('deletes task from tasks', () => {
+    const oldState = {
+      tasks: {
+        0: { title: 'root', subTasks: ['1'] },
+        1: { title: '첫번째 할일', subTasks: [] },
+      },
+    };
+    const newState = {
+      tasks: { 0: { title: 'root', subTasks: [] } },
+    };
 
-      const newState = {
-        currentTaskId: '1',
-        nextTaskId: '3',
-        tasks: {
-          1: {
-            title: '책 읽기',
-            children: {
-              2: { title: '애자일 공부', children: {} },
-            },
-          },
-        },
-      };
-
-      expect(reducer(
-        oldState,
-        addTask({ title: '애자일 공부', children: {} }),
-      )).toEqual(newState);
-    });
-
-    it('updates currentTaskId', () => {
-      const oldState = {
-        currentTaskId: '1',
-      };
-      const newState = {
-        currentTaskId: '3',
-      };
-
-      expect(reducer(
-        oldState,
-        updateCurrentTaskId('3'),
-      )).toEqual(newState);
-    });
-  });
-
-  context('when tasks to delete is in depth 0', () => {
-    it('deletes task from tasks', () => {
-      const oldState = {
-        tasks: {
-          1: { title: '책 읽기1', children: {} },
-          2: { title: '책 읽기2', children: {} },
-        },
-      };
-      const newState = {
-        tasks: { 1: { title: '책 읽기1', children: {} } },
-      };
-
-      expect(reducer(
-        oldState,
-        deleteTask('2'),
-      )).toEqual(newState);
-    });
-  });
-
-  context('when tasks to delete is in depth 1', () => {
-    it('deletes task from tasks and remove id in chidren', () => {
-      const oldState = {
-        tasks: {
-          1: {
-            title: '책 읽기',
-            children: {
-              3: { title: '서브 테스크 1', children: {} },
-              5: { title: '서브 테스크 2', children: {} },
-            },
-          },
-
-          2: {
-            title: '애자일 공부',
-            children: {
-              4: { title: '서브 테스크 3', children: {} },
-              6: { title: '서브 테스크 4', children: {} },
-            },
-          },
-        },
-      };
-
-      const newState = {
-        tasks: {
-          1: {
-            title: '책 읽기',
-            children: {
-              3: { title: '서브 테스크 1', children: {} },
-              5: { title: '서브 테스크 2', children: {} },
-            },
-          },
-          2: {
-            title: '애자일 공부',
-            children: {
-              6: { title: '서브 테스크 4', children: {} },
-            },
-          },
-        },
-      };
-
-      expect(reducer(
-        oldState,
-        deleteTask('4'),
-      )).toEqual(newState);
-    });
+    expect(reducer(
+      oldState,
+      deleteTask('1'),
+    )).toEqual(newState);
   });
 });
