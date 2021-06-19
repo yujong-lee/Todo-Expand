b0b7b1d5633fba98168976fea0919339cfb04cca
yujonglee <yujonglee@g.skku.edu>
2021-06-19 17:20:05 +0900

    SubTasks테스트를 추가하라

---
+++ src/components/SubTasks.test.jsx
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react';
+import { useSelector } from 'react-redux';
+
+import SubTasks from './SubTasks';
+
+describe('SubTasks', () => {
+  it('renders', () => {
+    useSelector.mockImplementation((selector) => selector({
+      todo: {
+        tasks: {
+          1: { title: 'task1', subTasks: [] },
+          2: { title: 'task2', subTasks: [] },
+        },
+      },
+    }));
+
+    render(<SubTasks subTasks={['1', '2']} />);
+  });
+});
