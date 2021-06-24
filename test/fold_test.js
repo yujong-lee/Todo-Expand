0e6489858b28e1ea01b1afe863836e23d6ee6727
yujonglee <yujonglee@g.skku.edu>
2021-06-24 11:20:20 +0900

    의도를 더 잘 드러내도록 fold_test를 다시 작성하라

--- test/fold_test.js
+++ test/fold_test.js
@@ -6,21 +6,27 @@ Scenario('can fold/unfold tasks', ({ I }) => {
     I.fillField('할 일', '첫 번째 할 일');
     I.click('추가');
   
+    I.click('첫 번째 할 일');
     I.fillField('할 일', '두 번째 할 일');
     I.click('추가');
-  
-    I.click('첫 번째 할 일');
+
+    I.click('두 번째 할 일');
     I.fillField('할 일', '세 번째 할 일');
     I.click('추가');
 
-    I.fillField('할 일', '네 번째 할 일');
-    I.click('추가');
+
   
-    I.click('접기');
+    I.click('//*[@id="app"]/div[1]/ul/li/ul/li/button[2]');
     I.dontSee('세 번째 할 일');
-    I.dontSee('네 번째 할 일');
+
+
+    I.click('//*[@id="app"]/div[1]/ul/li/button[2]');
+    I.dontSee('두 번째 할 일');
+
+    I.click('펼치기');
+    I.see('두 번째 할 일');
+    I.dontSee('세 번째 할 일');
 
     I.click('펼치기');
     I.see('세 번째 할 일');
-    I.see('네 번째 할 일');
 });
